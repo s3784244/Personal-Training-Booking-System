da4a6be7cb8cc6cfaa7c4438afc3629f8187b2e8
Kajal Soni <s3784244@student.rmit.edu.au>
2024-12-06 23:15:56 +1100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import authRoute from './Routes/auth.js'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+// database connection
+mongoose.set('strictQuery', false)
+const connectDB = async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,  
+    })
+
+    console.log('mongodb database is connected')
+
+  } catch (err) {
+    console.log('mongo database connection failed')
+    
+  }
+}
+
+app.get ('/', (req, res) => {
+  res.send('Api is working')
+})
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use('/api/v1/auth', authRoute)
+
+app.listen(port, () => {
+  connectDB();
+  console.log('server is running on port' + port)
+})
