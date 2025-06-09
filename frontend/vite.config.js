983beab3b6cb87732f6595a8b3c5c8a5290f85f0
Kajal Soni <s3784244@student.rmit.edu.au>
2025-06-09 17:27:33 +1000

    Revert "updated files for deployment"
    This reverts commit 5b7d166b02f78e612705c1751cb919224dc65490.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  define: {
-    'process.env.NODE_ENV': JSON.stringify('production')
-  }
 })
